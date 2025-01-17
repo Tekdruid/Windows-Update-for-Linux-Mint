b539f0ad0bed24bbe3f639f5af28c7d73b74a85b
Aki Laukkanen <aki.sus.laukkanen@gmail.com>
2025-01-17 10:37:40 +0200

    Added parameter passing
    Added test to see if first parameter is '-r' or 'reboot'.

--- WU.sh
+++ WU.sh
@@ -1,1 +1,7 @@
-apt-get update && apt-get upgrade --yes && shutdown now
+apt-get update && apt-get upgrade --yes 
+&&
+if [$1 = '-r'] || [$1 = '--reboot']
+    reboot now
+else
+    shutdown now
+fi
