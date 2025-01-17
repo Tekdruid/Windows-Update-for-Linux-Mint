143ef6f9e2133e17137ddbb3214fd5e4d91d28e1
Aki Laukkanen <aki.sus.laukkanen@gmail.com>
2025-01-17 11:01:41 +0200

    Added OR condition
    Added OR condition to test if parameter is "--reboot".

--- WU.sh
+++ WU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get upgrade --yes 
 # echo  $1
-if [[ $1 == '-r' ]]; then
+if [[ $1 == '-r' || $1 == '--reboot' ]]; then
     # echo "reboot"
     reboot now 
 else
